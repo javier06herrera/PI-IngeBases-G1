0dbd124b908ad0709a6554d821e50cbf5645ce4b
Javier Herrera <javier06herrera@gmail.com>
2020-06-08 00:17:42 -0600

    Database documentation

--- DB/Anexo E-Creación de Tablas.sql
+++ DB/Anexo E-Creación de Tablas.sql
@@ -1,3 +1,13 @@
+DROP TABLE HAS_SKILL;
+DROP TABLE Skill;
+DROP TABLE WRITES;
+DROP TABLE REVIEWS;
+DROP TABLE Question;
+DROP TABLE CommunityMember;
+DROP TABLE INVOLVES;
+DROP TABLE Article;
+DROP TABLE Topic;
+
 CREATE TABLE Article( 
 articleId		INT IDENTITY(1,1) PRIMARY KEY,
 name			VARCHAR(200) NOT NULL UNIQUE,
@@ -10,15 +20,16 @@ accessCount		INT NOT NULL DEFAULT 0,
 likesCount		INT NOT NULL DEFAULT 0, 
 dislikesCount	INT NOT NULL DEFAULT 0,
 likeBalance		INT NOT NULL DEFAULT 0,
+
 );
 
 
 
 insert into Article
-values ('Robinson Crusoe','short','Wreck of an english man and survival in a desert island','12-02-1999','C://Daniel/Defoe',DEFAULT,DEFAULT,DEFAULT,DEFAULT,DEFAULT);
+values ('Robinson Crusoe','short','Wreck of an english man and survival in a desert island','12-02-1999','Nací en 1632, en la ciudad de York, de una buena familia, aunque no de la región, pues mi padre era un extranjero de Brema1 que, inicialmente, se asentó en Hull2. Allí consiguió hacerse con una considerable fortuna como comerciante y, más tarde, abandonó sus negocios y se fue a vivir a York, donde se casó con mi madre, que pertenecía a la familia Robinson, una de las buenas familias del condado de la cual obtuve mi nombre, Robinson Kreutznaer. Mas, por la habitual alteración de las palabras que se hace en Inglaterra, ahora nos llaman y nosotros también nos llamamos y escribimos nuestro nombre Crusoe; y así me han llamado siempre mis compañeros.   Tenía dos hermanos mayores, uno de ellos fue coronel de un regimiento de infantería inglesa en Flandes, que antes había estado bajo el mando del célebre coronel Lockhart, y murió en la batalla de Dunkerque3 contra los españoles.    Lo que fue de mi segundo hermano, nunca lo he sabido al igual que mi padre y mi madre tampoco supieron lo que fue de mí.',DEFAULT,DEFAULT,DEFAULT,DEFAULT,DEFAULT);
 
 insert into Article
-values ('I, Robot','long','Elaborate philosophic implications concerning the three laws of robotics','12-02-1999','C://Isaac/Asimov',DEFAULT,DEFAULT,DEFAULT,DEFAULT,DEFAULT);
+values ('I, Robot','long','Elaborate philosophic implications concerning the three laws of robotics','12-02-1999','Un  robot  no  debe  dañar  a  un  ser  humano  o,  por  su  inacción,dejar que un ser humano sufra daño.2.  Un  robot  debe  obedecer  las  órde  nes  que  le  son  dadas  por  unser  hu  mano,  excepto  cuando  estas  órdenes  están  en  oposicióncon la primera Ley.3.  Un  robot  debe  proteger  su  propia  existencia,  hasta  donde  estaprotec ción no esté en conflicto con la primera o segunda Leyes.',DEFAULT,DEFAULT,DEFAULT,DEFAULT,DEFAULT);
 
 insert into Article
 values ('Mamita Yunai','long','Life of a "criollo" politician and his fight against an oppresive fruit company','12-02-1999','C://Carlos/Fallas',DEFAULT,DEFAULT,DEFAULT,DEFAULT,DEFAULT);
@@ -26,9 +37,9 @@ values ('Mamita Yunai','long','Life of a "criollo" politician and his fight agai
 ---------------------------------------------------------------------------------------------------------------------------------
 
 CREATE TABLE Topic(
-subject_category  VARCHAR(100),
-subject_topicName VARCHAR(100),
-PRIMARY KEY(subject_category, subject_topicName)
+subjectCategory  VARCHAR(100),
+subjectTopicName VARCHAR(100),
+PRIMARY KEY(subjectCategory, subjectTopicName)
 )
 
 INSERT INTO	Topic
@@ -50,7 +61,7 @@ category  VARCHAR(100),
 topicName VARCHAR(100),
 PRIMARY KEY(articleId, category, topicName),
 CONSTRAINT FK_Article_INVOLVES FOREIGN KEY (articleId) REFERENCES Article(articleID) ON DELETE CASCADE ON UPDATE CASCADE,
-CONSTRAINT FK_TopicCat_INVOLVES FOREIGN KEY (category, topicName) REFERENCES Topic(subject_category, subject_topicName) ON DELETE NO ACTION ON UPDATE CASCADE
+CONSTRAINT FK_TopicCat_INVOLVES FOREIGN KEY (category, topicName) REFERENCES Topic(subjectCategory, subjectTopicName) ON DELETE NO ACTION ON UPDATE CASCADE
 )
 
 INSERT INTO INVOLVES
@@ -70,39 +81,40 @@ CREATE TABLE CommunityMember(
 memberId			INT IDENTITY(1,1) PRIMARY KEY,
 name				VARCHAR(75) NOT NULL,
 lastName			VARCHAR(75) NOT NULL,
-birthDate			DATE	NOT NULL,
-age					INT		NOT NULL,	
-addressCity		VARCHAR(150) NOT NULL,
+birthDate			DATE			 	,
+age					INT					,	
+addressCity			VARCHAR(150) NOT NULL,
 addressCountry		VARCHAR(150) NOT NULL,
-hobbies				VARCHAR(300) NOT NULL,
-languages			VARCHAR(300) NOT NULL,
+hobbies				VARCHAR(300) 		,
+languages			VARCHAR(300) 		,
 email				VARCHAR(100) NOT NULL,
-phoneNumber			VARCHAR(15) NOT NULL UNIQUE,
-workInformation		VARCHAR(MAX) NOT NULL,
-typeOfMember		VARCHAR(100) NOT NULL,
-totalQualification	INT	NOT NULL DEFAULT 0,
+mobile				VARCHAR(15) NOT NULL UNIQUE,
+job					VARCHAR(MAX) NOT NULL,
+memberRank			VARCHAR(100) 		,
+points				INT	NOT NULL DEFAULT 0,
+skills				VARCHAR(MAX) NOT NULL
 )
 
 INSERT INTO CommunityMember
-VALUES ( 'Kevin', 'Barrantes','10-10-1990', 30, 'Rio Segundo','Costa Rica','Read, Listen good music','Spanish, English','barrKev@puchimail.com','1','Intel','Generic',DEFAULT)
+VALUES ( 'Kevin', 'Barrantes','10-10-1990', 30, 'Rio Segundo','Costa Rica','Read, Listen good music','Spanish, English','barrKev@puchimail.com','1','Intel','Generic',DEFAULT,'Versatility')
 
 INSERT INTO CommunityMember
-VALUES ( 'Gloriana', 'Mora','10-10-1990', 30,'San Sebastián','Costa Rica','Read, travel','Spanish, English','moraGlo@puchimail.com','2','Intel','Generic', DEFAULT)
+VALUES ( 'Gloriana', 'Mora','10-10-1990', 30,'San Sebastián','Costa Rica','Read, travel','Spanish, English','moraGlo@puchimail.com','2','Intel','Generic', DEFAULT, 'Creativity')
 
 INSERT INTO CommunityMember
-VALUES ( 'Antonio', 'Álvares', '10-10-1990',30,'Barva','Costa Rica','Play video games','Spanish, English','alvAnt@puchimail.com','3','Intel','Generic',DEFAULT)
+VALUES ( 'Antonio', 'Álvares', '10-10-1990',30,'Barva','Costa Rica','Play video games','Spanish, English','alvAnt@puchimail.com','3','Intel','Generic',DEFAULT,'Math-skills')
 
 INSERT INTO CommunityMember
-VALUES ( 'Daniel', 'Barrante','10-10-1990', 30,'Alajuela','Costa Rica','Play the piano, eat caldosas, beign a "bicho"','Spanish, English','barrDan@puchimail.com','4','Intel','Generic',DEFAULT)
+VALUES ( 'Daniel', 'Barrante','10-10-1990', 30,'Alajuela','Costa Rica','Play the piano, eat caldosas, beign a "bicho"','Spanish, English','barrDan@puchimail.com','4','Intel','Generic',DEFAULT,'Adaptability')
 
 INSERT INTO CommunityMember
-VALUES ( 'Javier', 'Herrera','10-10-1990', 30, 'Santa Ana','Costa Rica','Play the guitar','Spanish, English','herrJav@puchimail.com','5','Intel','Generic',DEFAULT)
+VALUES ( 'Javier', 'Herrera','10-10-1990', 30, 'Santa Ana','Costa Rica','Play the guitar','Spanish, English','herrJav@puchimail.com','5','Intel','Generic',DEFAULT,'Organized')
 
 ---------------------------------------------------------------------------------------------------------------------------------
 CREATE TABLE Skill(
-subject_category  VARCHAR(100),
-subject_skillName VARCHAR(100),
-PRIMARY KEY(subject_category, subject_skillName)
+subjectCategory  VARCHAR(100),
+subjectSkillName VARCHAR(100),
+PRIMARY KEY(subjectCategory, subjectSkillName)
 )
 
 INSERT INTO Skill
@@ -118,7 +130,7 @@ category  VARCHAR(100),
 skillName VARCHAR(100),
 PRIMARY KEY(memberId, category, skillName),
 CONSTRAINT FK_Article_HAS_SKILL FOREIGN KEY (memberId) REFERENCES CommunityMember(memberId) ON DELETE CASCADE ON UPDATE CASCADE,
-CONSTRAINT FK_SkillCat_HAS_SKILL FOREIGN KEY (category, skillName) REFERENCES Skill(subject_category, subject_skillName) ON DELETE CASCADE ON UPDATE CASCADE
+CONSTRAINT FK_SkillCat_HAS_SKILL FOREIGN KEY (category, skillName) REFERENCES Skill(subjectCategory, subjectSkillName) ON DELETE CASCADE ON UPDATE CASCADE
 )
 
 INSERT INTO HAS_SKILL
@@ -145,10 +157,10 @@ CONSTRAINT FK_CommunityMember_Question FOREIGN KEY (memberId) REFERENCES Communi
 );
 
 INSERT INTO	Question
-VALUES (1, 'Why are we here?','posted', DEFAULT, DEFAULT)
+VALUES (1, 'How to exit VIM?','posted', 'If you are in edit mode, first press the <Esc> key. Then enter :wq + <Enter> to save and exit. And by the way this does not work', DEFAULT)
 
 INSERT INTO	Question
-VALUES ( 2,'What happened to Jimmy Hoffa?',DEFAULT, DEFAULT, DEFAULT)
+VALUES ( 2,'How to create an article?',DEFAULT, DEFAULT, DEFAULT)
 
 INSERT INTO	Question
 VALUES (3, 'Will vaccinations make me autistic?', DEFAULT , 'No', 'checked')
@@ -163,6 +175,9 @@ CONSTRAINT FK_Article_WRITES FOREIGN KEY (articleId) REFERENCES Article(articleI
 
  INSERT INTO WRITES
  VALUES	(1,1)
+
+ INSERT INTO WRITES
+ VALUES	(1,2)
 
  INSERT INTO WRITES
  VALUES	(2,1)
