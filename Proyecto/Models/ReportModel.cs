86f34179030c28c541d23f3052c4441ef728b68d
KevinBarrantes <kbarransa@hotmail.com>
2020-06-24 01:52:16 -0600

    CheckBox for categories added to project

---
+++ Proyecto/Models/ReportModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using System.Runtime.Serialization;
+
+namespace Proyecto.Models
+{
+
+    [DataContract]
+    public class ReportModel
+    {
+        public string[] selectedCategory { get; set; }
+        public List<SelectListItem> CategoryList { get; set; }
+        public int count { get; set; }
+        public string value { get; set; }
+
+    }
+}
