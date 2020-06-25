b0e37d7bab6c1778c5f0b7936f3577b9a22e013a
glorianamv <gloriana.moravillalta@ucr.ac.cr>
2020-06-25 10:32:26 -0600

    Dropdown list with checkboxes in order to select the desiredfilters in the report

---
+++ Proyecto/Controllers/CommunityProgressReportController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Proyecto.Models;
+
+namespace Proyecto.Controllers
+{
+    public class CommunityProgressReportController : Controller
+    {
+        public ActionResult Index()
+        {
+            var model = new CommunityProgressReportModel
+            {
+                SelectedFiltersIds = new[] { 2 },
+                Filters = GetAllFilters()
+            };
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Index(CommunityProgressReportModel model)
+        {
+            model.Filters = GetAllFilters();
+            if (model.SelectedFiltersIds != null)
+            {
+                List<SelectListItem> selectedItems = model.Filters.Where(p => model.SelectedFiltersIds.Contains(int.Parse(p.Value))).ToList();
+                foreach (var filter in selectedItems)
+                {
+                    filter.Selected = true;
+                    ViewBag.Message += filter.Text + " | ";
+                }
+            }
+            return View(model);
+        }
+
+        public List<SelectListItem> GetAllFilters()
+        {
+            List<SelectListItem> items = new List<SelectListItem>();
+            items.Add(new SelectListItem { Text = "Article Count", Value = "1" });
+            items.Add(new SelectListItem { Text = "Article Points", Value = "2" });
+            items.Add(new SelectListItem { Text = "Article Count Peer Category And Topic", Value = "3" });
+            items.Add(new SelectListItem { Text = "Access Count Peer Category And Topic", Value = "4" });
+            return items;
+        }
+
+    }
+}
---
+++ Proyecto/Models/CommunityProgressReportModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Proyecto.Models
+{
+    public class CommunityProgressReportModel
+    {
+        public int[] SelectedFiltersIds { get; set; }
+        public IEnumerable<SelectListItem> Filters { get; set; }
+    }
+}
