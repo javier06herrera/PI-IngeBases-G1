e5bcde0ef546f64af483f29cba4ae102be9a7957
glorianamv <gloriana.moravillalta@ucr.ac.cr>
2020-06-25 16:44:38 -0600

    Fixed dropdown with check boxes to select member rank and filters. Added libraries to create graphics.

--- Proyecto/Controllers/CommunityProgressReportController.cs
+++ Proyecto/Controllers/CommunityProgressReportController.cs
@@ -9,41 +9,16 @@ namespace Proyecto.Controllers
 {
     public class CommunityProgressReportController : Controller
     {
+        [HttpGet]
         public ActionResult Index()
         {
-            var model = new CommunityProgressReportModel
-            {
-                SelectedFiltersIds = new[] { 2 },
-                Filters = GetAllFilters()
-            };
-            return View(model);
+            return View();
         }
 
         [HttpPost]
         public ActionResult Index(CommunityProgressReportModel model)
         {
-            model.Filters = GetAllFilters();
-            if (model.SelectedFiltersIds != null)
-            {
-                List<SelectListItem> selectedItems = model.Filters.Where(p => model.SelectedFiltersIds.Contains(int.Parse(p.Value))).ToList();
-                foreach (var filter in selectedItems)
-                {
-                    filter.Selected = true;
-                    ViewBag.Message += filter.Text + " | ";
-                }
-            }
-            return View(model);
+            return View();
         }
-
-        public List<SelectListItem> GetAllFilters()
-        {
-            List<SelectListItem> items = new List<SelectListItem>();
-            items.Add(new SelectListItem { Text = "Article Count", Value = "1" });
-            items.Add(new SelectListItem { Text = "Article Points", Value = "2" });
-            items.Add(new SelectListItem { Text = "Article Count Peer Category And Topic", Value = "3" });
-            items.Add(new SelectListItem { Text = "Access Count Peer Category And Topic", Value = "4" });
-            return items;
-        }
-
     }
 }
---
+++ Proyecto/Scripts/js/CommunityProgressReport.js
@@ -0,0 +1,31 @@
+﻿function generateReport()
+{
+    var container = document.getElementById("canvasContainer");
+    var canvas = document.createElement("canvas");
+    getGraphic(canvas);
+    container.appendChild(canvas);
+}
+
+function getGraphic(canvas)
+{
+    new Chart(canvas, {
+        type: 'bar',
+        data: {
+            labels: ["Africa", "Asia", "Europe", "Latin America", "North America"],
+            datasets: [
+                {
+                    label: "Population (millions)",
+                    backgroundColor: ["#3e95cd", "#8e5ea2", "#3cba9f", "#e8c3b9", "#c45850"],
+                    data: [2478, 5267, 734, 784, 433]
+                }
+            ]
+        },
+        options: {
+            legend: { display: false },
+            title: {
+                display: true,
+                text: 'Predicted world population (millions) in 2050'
+            }
+        }
+    });
+}
