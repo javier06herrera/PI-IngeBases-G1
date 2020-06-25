0ef6d5350ff00a76b770e8bf886bf65891087f3d
glorianamv <gloriana.moravillalta@ucr.ac.cr>
2020-06-25 17:12:15 -0600

    Created scripts to get selected member ranks and filters

--- Proyecto/Scripts/js/CommunityProgressReport.js
+++ Proyecto/Scripts/js/CommunityProgressReport.js
@@ -1,9 +1,26 @@
 ﻿function generateReport()
 {
-    var container = document.getElementById("canvasContainer");
-    var canvas = document.createElement("canvas");
-    getGraphic(canvas);
-    container.appendChild(canvas);
+    var memberRankDropdown = document.getElementById("memberRanks");
+    var filtersDropdown = document.getElementById("filters");
+    var selectedMemberRanks = getSelectedItems(memberRankDropdown);
+    var selectedFilters = getSelectedItems(filtersDropdown);
+}
+
+function getSelectedItems(dropdownlist)
+{
+    var selectedItems = [];
+    var len = dropdownlist.options.length;
+    var currentOption = null;
+
+    for (var item = 0;  item < len; ++item)
+    {
+        currentOption = dropdownlist.options[item];
+        if (currentOption.selected)
+        {
+            selectedItems.push(currentOption);
+        }
+    }
+    return selectedItems;
 }
 
 function getGraphic(canvas)
