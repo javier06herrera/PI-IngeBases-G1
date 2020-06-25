52b2b3bd02037f63d5a2d9621ec3ec8f38e5a21d
KevinBarrantes <kbarransa@hotmail.com>
2020-06-25 00:51:18 -0600

    Users report with graphics added to project

--- Proyecto/Controllers/ReportController.cs
+++ Proyecto/Controllers/ReportController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Proyecto.Models;
+using Newtonsoft.Json;
 
 namespace Proyecto.Controllers
 {
@@ -14,18 +15,64 @@ namespace Proyecto.Controllers
         {
             ReportModel model = new ReportModel();
             model.CategoryList = new List<SelectListItem>();
-            model.CategoryList.Add(new SelectListItem() { Text = "Country", Value = "1" });
-            model.CategoryList.Add(new SelectListItem() { Text = "Skills", Value = "2" });
-            model.CategoryList.Add(new SelectListItem() { Text = "Languages", Value = "3" });
-            model.CategoryList.Add(new SelectListItem() { Text = "Hobbies", Value = "4" });
+            model.CategoryList.Add(new SelectListItem() { Text = "Country", Value = "Country" });
+            model.CategoryList.Add(new SelectListItem() { Text = "Skills", Value = "Skills" });
+            model.CategoryList.Add(new SelectListItem() { Text = "Languages", Value = "Languages" });
+            model.CategoryList.Add(new SelectListItem() { Text = "Hobbies", Value = "Hobbies" });
             return View(model);
         }
 
         [HttpPost]
         public ActionResult ReportUsers(ReportModel model)
         {
-            return View();
+            ReportDBHandle sdb = new ReportDBHandle();
+            string query;
+            if (model.selectedCategory.Contains("Country"))
+            {
+                query = "SELECT C.addressCountry As 'Value', (Count(*)) As 'Count' " +
+                                " FROM CommunityMember C " +
+                                " Group By C.addressCountry ";
+                List<ReportModel> dataPoints = sdb.GetCountryStats(query);
+                ViewBag.Country = JsonConvert.SerializeObject(dataPoints);
+            }
+
+            if (model.selectedCategory.Contains("Skills"))
+            {
+                query = "SELECT H.skillName As 'Value', (Count(*)) As 'Count' " +
+                                " FROM CommunityMember C, HAS_SKILL H, Skill S " +
+                                " WHERE C.memberId = H.memberId " +
+                                " AND H.category = S.subject_category " +
+                                " AND H.skillName = subject_skillName " +
+                                " Group By H.skillName ";
+                List<ReportModel> dataPoints = sdb.GetCountryStats(query);
+                ViewBag.Skills = JsonConvert.SerializeObject(dataPoints);
+            }
+
+
+            if (model.selectedCategory.Contains("Languages"))
+            {
+                query = "SELECT C.languages As 'Value', (Count(*)) As 'Count' " +
+                                " FROM CommunityMember C " +
+                                " Group By C.languages ";
+                List<ReportModel> dataPoints = sdb.GetCountryStats(query);
+                ViewBag.Languages = JsonConvert.SerializeObject(dataPoints);
+            }
+
+            if (model.selectedCategory.Contains("Hobbies"))
+            {
+                query = "SELECT C.hobbies As 'Value', (Count(*)) As 'Count' " +
+                                " FROM CommunityMember C " +
+                                " Group By C.hobbies ";
+                List<ReportModel> dataPoints = sdb.GetCountryStats(query);
+                ViewBag.Hobbies = JsonConvert.SerializeObject(dataPoints);
+            }
+
+            model.CategoryList = new List<SelectListItem>();
+            model.CategoryList.Add(new SelectListItem() { Text = "Country", Value = "Country" });
+            model.CategoryList.Add(new SelectListItem() { Text = "Skills", Value = "Skills" });
+            model.CategoryList.Add(new SelectListItem() { Text = "Languages", Value = "Languages" });
+            model.CategoryList.Add(new SelectListItem() { Text = "Hobbies", Value = "Hobbies" });
+            return View(model);
         }
-        //http://localhost:54007/Report/ReportUsers
     }
 }
