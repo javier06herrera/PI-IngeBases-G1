0513a30cadc2407d96954b5cf781665767bf5635
Antonio Álvarez <antonio.alvarez.chavarria@gmail.com>
2020-06-26 14:45:50 +0200

    The view to ask colaboration was added

---
+++ Proyecto/Controllers/ColaborationController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Proyecto.Models;
+
+
+namespace Proyecto.Controllers
+{
+    public class ColaborationController : Controller
+    {
+        // GET: Colaboration
+        public ActionResult Colaboration()
+        {
+            ArticleDBHandle dbhandle = new ArticleDBHandle();
+            ModelState.Clear();
+            return View(dbhandle.GetArticle());
+        }
+
+
+        //public ActionResult askColaboration(ArticleModel model)
+        //{
+
+        //    return View();
+        //}
+
+        //[HttpPost]
+        public ActionResult askColaboration(ArticleModel model)
+        {
+            ///cosas por hacer 
+
+            return RedirectToAction("Colaboration");
+        }
+
+
+    }
+}
