d8c44b2e4c8c3e1ac762ce1d379e27f2ae584ab9
glorianamv <gloriana.moravillalta@ucr.ac.cr>
2020-06-26 13:05:20 -0600

    New script to create multiple canvas through html code dinamically generated, according to selected filters. Return of values to graphicate in progress.

--- Proyecto/Scripts/js/CommunityProgressReport.js
+++ Proyecto/Scripts/js/CommunityProgressReport.js
@@ -1,14 +1,18 @@
-﻿function generateReport()
+﻿function initReport()
 {
+    // Obtain dropdown objects.
     var memberRankDropdown = document.getElementById("memberRanks");
     var filtersDropdown = document.getElementById("filters");
+    // Obtain selected items in dropdowns.
     var selectedMemberRanks = getSelectedItems(memberRankDropdown);
     var selectedFilters = getSelectedItems(filtersDropdown);
-    var values = getValues(selectedMemberRanks, selectedFilters);
+    // Generate report according to selected items.
+    generateReport(selectedMemberRanks, selectedFilters);
 }
 
 function getSelectedItems(dropdownlist)
 {
+    // Return an array of strings containing the selected options of the given dropdownlist.
     var selectedItems = [];
     var len = dropdownlist.options.length;
     var currentOption = null;
@@ -24,39 +28,67 @@ function getSelectedItems(dropdownlist)
     return selectedItems;
 }
 
-function getValues(selectedMemberRanks, selectedFilters)
+function generateReport(selectedMemberRanks, selectedFilters)
 {
-    var valuesList = null;
+    var values = null;
+    var canvas = null;
+    // For each filter, get the required data, create a canvas and graphicate it.
+    for (var filter = 0; filter < selectedFilters.length; ++filter)
+    {
+        values = getFilteredValues(selectedMemberRanks, filter);
+        canvas = createCanvas();
+        drawGraphics(canvas, values);
+    }
+}
 
+function getFilteredValues(selectedMemberRanks, filter)
+{
+    var filteredValues = null; 
     $.ajax({
-        url: '/CommunityProgressReport/GetValues',
-        data:
-        {
+        url: '/CommunityProgressReport/GetFilteredValues',
+        data: {
             selectedMemberRanks: selectedMemberRanks,
-            selectedFilters: selectedFilters
+            filter: filter,
         },
         type: 'post',
         dataType: 'json',
         async: false,
-        success: function (results) 
-        {
-            valuesList = results;
+        success: function (results) {
+            filteredValues = results;
         }
     });
-    return valuesList;
+    return filteredValues;
 }
 
-function getGraphic(canvas)
+function createCanvas()
+{
+    // Generate required html elements inside a predefined row, in order to place a canvas. 
+    var row = document.getElementById("row");
+    var colDiv = document.createElement("div");
+    colDiv.setAttribute("class", "col-md-6 py-1");
+    var cardDiv = document.createElement("div");
+    cardDiv.setAttribute("class", "card");
+    var cardBodyDiv = document.createElement("div");
+    cardBodyDiv.setAttribute("class", "card-body");
+    var canvas = document.createElement("canvas");
+    cardBodyDiv.appendChild(canvas);
+    cardDiv.appendChild(cardBodyDiv);
+    colDiv.appendChild(cardDiv);
+    row.appendChild(colDiv); 
+    return canvas;
+}
+
+function drawGraphics(canvas, values)
 {
     new Chart(canvas, {
-        type: 'bar',
+        type: 'doughnut',
         data: {
             labels: ["Africa", "Asia", "Europe", "Latin America", "North America"],
             datasets: [
                 {
                     label: "Population (millions)",
                     backgroundColor: ["#3e95cd", "#8e5ea2", "#3cba9f", "#e8c3b9", "#c45850"],
-                    data: [2478, 5267, 734, 784, 433]
+                    data: values;
                 }
             ]
         },
@@ -65,7 +97,15 @@ function getGraphic(canvas)
             title: {
                 display: true,
                 text: 'Predicted world population (millions) in 2050'
-            }
+            },
+            responsive: true,
+            maintainAspectRatio: true,
+            devicePixelRatio: 2
         }
     });
 }
+
+
+
+
+
