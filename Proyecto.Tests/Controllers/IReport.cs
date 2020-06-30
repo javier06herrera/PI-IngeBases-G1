4cee9aa338d5e38c9f5c2844914941062971a8c5
KevinBarrantes <kbarransa@hotmail.com>
2020-06-30 14:48:35 -0600

    Tests added to project

---
+++ Proyecto.Tests/Controllers/IReport.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Proyecto.Controllers;
+using Proyecto.Models;
+
+namespace Proyecto.Tests.Controllers
+{
+    interface IReport
+    {
+        //List<ReportModel> GetCountryStats(string query);
+        List<ReportModel> GetCountryStats { get; set; }
+    }
+}
---
+++ Proyecto.Tests/Controllers/IReview.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace Proyecto.Tests.Controllers
+{
+    public interface IReview
+    {
+        int articleId { get; set; }
+        string email { get; set; }
+        string comments { get; set; }
+        int generalOpinion { get; set; }
+        int communityContribution { get; set; }
+        int articleStructure { get; set; }
+        int totalGrade { get; set; }        
+        string state { get; set; }
+    }
+}
