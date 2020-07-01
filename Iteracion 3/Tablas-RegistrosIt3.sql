59b1cbd1423d6086ea94d57191fe7f5e8be17b48
Antonio Álvarez <antonio.alvarez.chavarria@gmail.com>
2020-07-01 11:44:04 -0600

    Fix on the views count

--- Iteracion 3/Tablas-RegistrosIt3.sql
+++ Iteracion 3/Tablas-RegistrosIt3.sql
@@ -298,11 +298,11 @@ CONSTRAINT FK_CommunityMember_IS_NOMINATED FOREIGN KEY (email) REFERENCES Commun
 CONSTRAINT FK_Article_IS_NOMINATED FOREIGN KEY (articleId) REFERENCES Article(articleId) ON DELETE CASCADE ON UPDATE CASCADE -- No se desea tener miembros nominados a revisar artículos que ya no existen. 
 )
 
-INSERT INTO IS_NOMINATED
-VALUES ('pending','no comment','barrKev@puchimail.com', 5)
+--INSERT INTO IS_NOMINATED
+--VALUES ('pending','no comment','barrKev@puchimail.com', 5)
 
-INSERT INTO IS_NOMINATED
-VALUES ('pending','no comment','antonio.alvarez.chavarria@gmail.com', 7)
+--INSERT INTO IS_NOMINATED
+--VALUES ('pending','no comment','antonio.alvarez.chavarria@gmail.com', 7)
 
 ---------------------------------------------------------------------------------------------
 
@@ -344,48 +344,52 @@ VALUES ('pending','no comment','antonio.alvarez.chavarria@gmail.com', 7)
 --WHERE W.email = @email
 --ORDER BY publishDate DESC
 
----------------------------------------------------------------
---CREATE TRIGGER PIT_totalCountUpdate
---ON Article
---FOR  UPDATE
---AS
+-------------------------------------------------------------
+GO
+CREATE TRIGGER PIT_totalCountUpdate
+ON Article
+FOR  UPDATE
+AS
 
---DECLARE @articleId int
---DECLARE @newLikeBalance int
---DECLARE @oldLikeBalance int
---select @articleId=I.articleId, @newLikeBalance = I.likesCount - I.dislikesCount
---from inserted I
---select @oldLikeBalance = D.likeBalance
---from deleted D
+DECLARE @articleId int
+DECLARE @newLikeBalance int
+DECLARE @oldLikeBalance int
+select @articleId=I.articleId, @newLikeBalance = I.likesCount - I.dislikesCount
+from inserted I
+select @oldLikeBalance = D.likeBalance
+from deleted D
 
---IF UPDATE(likesCount) OR UPDATE(dislikesCount)
---BEGIN
---	UPDATE Article
---	SET likeBalance = likesCount-dislikesCount
---	WHERE articleId = @articleId 
+IF UPDATE(likesCount) OR UPDATE(dislikesCount)
+BEGIN
+	UPDATE Article
+	SET likeBalance = likesCount-dislikesCount
+	WHERE articleId = @articleId 
 
---	UPDATE	CommunityMember
---	SET Points = Points + @newLikeBalance - @oldLikeBalance
---	WHERE email in (
---		SELECT W.email
---		FROM WRITES W
---		WHERE W.articleId = @articleId)
---END
----------------------------------------------------------------------------
---CREATE TRIGGER PIT_viewsMeritUpdate
---ON Article
---FOR  UPDATE
---AS
---DECLARE @articleId int
---SELECT @articleId = I.articleId
---FROM inserted I
+	UPDATE	CommunityMember
+	SET Points = Points + @newLikeBalance - @oldLikeBalance
+	WHERE email in (
+		SELECT W.email
+		FROM WRITES W
+		WHERE W.articleId = @articleId)
+END
+GO
+-------------------------------------------------------------------------
+GO
+CREATE TRIGGER PIT_viewsMeritUpdate
+ON Article
+FOR  UPDATE
+AS
+DECLARE @articleId int
+SELECT @articleId = I.articleId
+FROM inserted I
 
---IF UPDATE(accessCount)
---BEGIN
---	UPDATE CommunityMember
---	SET points = points + 1
---	WHERE email IN (
---			SELECT W.email
---			FROM WRITES W
---			WHERE W.articleId = @articleId)
---END 
+IF UPDATE(accessCount)
+BEGIN
+	UPDATE CommunityMember
+	SET points = points + 1
+	WHERE email IN (
+			SELECT W.email
+			FROM WRITES W
+			WHERE W.articleId = @articleId)
+END 
+GO
